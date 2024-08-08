f295f12f0db4eb5b0ae62da84f6cfd25fd911ce5
emmaotuonye <emmanuel.otuonye@dlhosolutions.ng>
2024-08-08 01:22:55 +0100

    added enumerator info on survey form

--- src/pages/admin/Survey.jsx
+++ src/pages/admin/Survey.jsx
@@ -2,17 +2,20 @@ import { useQuery } from "@tanstack/react-query";
 import React from "react";
 import { getSurveys } from "../../lib/service";
 import { GeneralTable } from "../../components/charts";
+import { transformSurveyData } from "../../lib/utils";
 
 const Survey = () => {
-  const { data } = useQuery({
+  const { data, isSuccess } = useQuery({
     queryKey: ["get-survey"],
     queryFn: () => getSurveys(),
   });
 
+  const tData = isSuccess ? transformSurveyData(data.data) : [];
+
   return (
     <div>
       <GeneralTable
-        data={data?.data || []}
+        data={tData}
         title={"Survey Table"}
         hiddenFields={["approved"]}
       />
